20b748b9632b3e47bdb238c7f1e949ad26c968c5
faiza kiran <144409199+faizasafdar177@users.noreply.github.com>
2023-10-01 10:17:57 +0500

    Add files via upload

---
+++ hassan.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+void printH();
+void printA();
+void printS(); 
+void printN();
+int main()
+ {
+      printH();
+ 
+      printA();
+    
+      printS();   
+
+      printS();
+    
+      printA();
+     
+      printN();
+
+  
+  
+}
+void printH()
+{
+    cout << "H";
+}
+void printA()
+{
+    cout << "A";
+}
+
+void printS()
+{
+    cout << "S";
+}
+
+void printN()
+{
+    cout << "N";
+}
